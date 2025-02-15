a25a5dcb73bf639f01821fc9593893678b6a0f9c
Raiyan109 <kraiyan109@gmail.com>
2025-02-15 14:52:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Canvas } from '@react-three/fiber'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div id='canvas-container'>
+      <Canvas style={{ backgroundColor: 'deepskyblue' }}>
+
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
